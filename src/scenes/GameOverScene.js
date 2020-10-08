b6515c4b21dfc8301a772b134d02d873a3154920
ebeagusamuel <samuelebeagu@gmail.com>
2020-10-08 10:43:00 +0100

    add gameover scene

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -18,6 +18,55 @@ class GameOverScene extends Phaser.Scene {
         align: "center",
       }
     );
+
+    this.add.text(130, 60, `Your Score: ${window.global.score}`, {
+      fontFamily: "monospace",
+      fontSize: 20,
+      fontStyle: "bold",
+      color: "#fed141",
+      align: "center",
+    })
+
+    this.add.text(
+      140, 100, "Top scores", {
+        fontFamily: "monospace",
+        fontSize: 25,
+        fontStyle: "bold",
+        color: "#ffffff",
+        align: "center",
+      }
+    );
+
+    this.add.text(80, 130, "PlayerName          Score", {
+      fontFamily: "monospace",
+      fontSize: 20,
+      fontStyle: "bold",
+      color: "#fed141",
+      align: "center",
+    })
+
+    for(let i = 0; i < 10; i += 1){
+      this.add.text(80, 170 + (i * 20), "Seyitope                  100", {
+        fontFamily: "monospace",
+        fontSize: 16,
+        fontStyle: "bold",
+        color: "#ffffff",
+        align: "center",
+      })
+    }
+
+    this.restartButton = this.add.text(180, 400, "Restart", {
+      fontFamily: "monospace",
+      fontSize: 18,
+      fontStyle: "bold",
+      color: "#000000",
+      backgroundColor: "#fed141",
+      padding: 5,
+    })
+    this.restartButton.setInteractive()
+    this.restartButton.on('pointerup', () => {
+      this.scene.start('MainScene')
+    }, this)
   }
 }
 
